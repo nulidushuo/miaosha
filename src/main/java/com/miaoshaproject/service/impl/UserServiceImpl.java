c45a03c39ffa869275383130cfdc2749da583dcd
nulidushuo <2390583664@qq.com>
2019-08-09 17:23:26 +0800

    3.1

---
+++ src/main/java/com/miaoshaproject/service/impl/UserServiceImpl.java
@@ -0,0 +1,57 @@
+package com.miaoshaproject.service.impl;
+
+import com.miaoshaproject.dao.UserDOMapper;
+import com.miaoshaproject.dao.UserPasswordDOMapper;
+import com.miaoshaproject.dataobject.UserDO;
+import com.miaoshaproject.dataobject.UserPasswordDO;
+import com.miaoshaproject.service.UserService;
+import com.miaoshaproject.service.model.UserModel;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * @program: miaosha
+ * @description:
+ * @author: JIANG Ziyang
+ * @create: 2019-08-09 15:18
+ **/
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserDOMapper userDOMapper;
+
+    @Autowired
+    private UserPasswordDOMapper userPasswordDOMapper;
+
+    @Override
+    public UserModel getUerById(Integer id) {
+        //调用userdomapper获取到对应用户的dataobject
+        UserDO userDO = userDOMapper.selectByPrimaryKey(id);
+        if (userDO == null) {
+            return null;
+        }
+
+        //通过用户id获取对应的用户加密密码信息
+        UserPasswordDO userPasswordDO = userPasswordDOMapper.selectByUserId(userDO.getId());
+        return convertFromDataObject(userDO, userPasswordDO);
+
+    }
+
+    private UserModel convertFromDataObject(UserDO userDO, UserPasswordDO userPasswordDO) {
+        if (userDO == null) {
+            return null;
+        }
+
+        UserModel userModel= new UserModel();
+        BeanUtils.copyProperties(userDO, userModel);
+
+        if (userPasswordDO != null) {
+            userModel.setEncrptPassword(userPasswordDO.getEncrptPassword());
+        }
+
+        return userModel;
+
+    }
+}
