42e2ca131f1300c60e780c54cd20b61e63483d79
nulidushuo <2390583664@qq.com>
2019-08-09 15:02:00 +0800

    2.4

--- src/main/java/com/miaoshaproject/App.java
+++ src/main/java/com/miaoshaproject/App.java
@@ -1,7 +1,12 @@
 package com.miaoshaproject;
 
+import com.miaoshaproject.dao.UserDOMapper;
+import com.miaoshaproject.dataobject.UserDO;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,15 +15,23 @@ import org.springframework.web.bind.annotation.RestController;
  *
  */
 //
-@EnableAutoConfiguration
-//Sprintboot启动内嵌Tomcat并加载配置
+@SpringBootApplication(scanBasePackages = {"com.miaoshaproject"})
 @RestController
+@MapperScan("com.miaoshaproject.dao")
 public class App 
 {
+    @Autowired
+    private UserDOMapper userDOMapper;
 
     @RequestMapping("/")
     public String home() {
-        return "Hello, World";
+
+        UserDO userDo = userDOMapper.selectByPrimaryKey(1);
+        if (userDo == null) {
+            return "用户对象不存在";
+        } else {
+            return userDo.getName();
+        }
     }
 
     public static void main( String[] args )
