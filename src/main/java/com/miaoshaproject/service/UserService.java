6a210aa250c7e69729a1103bd3e8ef21c0bbe9ab
nulidushuo <2390583664@qq.com>
2019-08-13 21:00:40 +0800

    3.12

--- src/main/java/com/miaoshaproject/service/UserService.java
+++ src/main/java/com/miaoshaproject/service/UserService.java
@@ -7,4 +7,10 @@ public interface UserService {
     //通过用户ID获取用户对象的方法
     UserModel getUerById(Integer id);
     void register(UserModel userModel)throws BusinessException;
+
+    /*
+    telphone:用户注册手机
+    password:用户加密后的密码
+     */
+    UserModel validateLogin(String telphone, String encrptPassword) throws BusinessException;
 }
--- src/main/java/com/miaoshaproject/service/impl/UserServiceImpl.java
+++ src/main/java/com/miaoshaproject/service/impl/UserServiceImpl.java
@@ -75,6 +75,25 @@ public class UserServiceImpl implements UserService {
         return;
     }
 
+    @Override
+    public UserModel validateLogin(String telphone, String encrptPassword) throws BusinessException {
+        //通过用户的手机获取用户信息
+        UserDO userDO = userDOMapper.selectByTelphone(telphone);
+        if(userDO == null){
+            throw new BusinessException(EmBusinessError.USER_LOGIN_FAIL);
+        }
+        UserPasswordDO userPasswordDO = userPasswordDOMapper.selectByUserId(userDO.getId());
+        UserModel userModel = convertFromDataObject(userDO, userPasswordDO);
+
+        //比对用户信息内加密的密码是否和传输进来的密码相匹配
+        if (!StringUtils.equals(encrptPassword, userModel.getEncrptPassword())){
+            throw new BusinessException(EmBusinessError.USER_LOGIN_FAIL);
+        }
+
+        return userModel;
+
+    }
+
     private UserPasswordDO convertPasswordFromModel(UserModel userModel){
         if(userModel == null){
             return null;
